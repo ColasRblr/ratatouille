8b7c0374d7efed26014d9bbed91f972a2c96369e
Colas Rabiller <colas.rabiller@gmail.com>
2023-09-29 14:28:44 +0200

    search input ok

--- renderer.js
+++ renderer.js
@@ -1,24 +1,41 @@
 const btn = document.querySelector('#btn');
+const form = document.querySelector('form');
+const list = document.querySelector('#list');
 
 const handleSearch = async (e) => {
+    const search = document.getElementById('search').value;
+
     e.preventDefault();
+    if(search != '')  {
+        try {
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
 
-    try {
-        const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+            }
 
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+            const data = await response.json();
+            console.log(data);
+            data.meals.forEach((meal) => {
+                console.log(meal);
+                let mealContainer = document.createElement("div");
+                list.appendChild(mealContainer);
+                mealContainer.innerHTML = meal.strMeal;
+                let image = document.createElement('img');
+                image.src = `${meal.strMealThumb}/preview`;
+                mealContainer.appendChild(image);
+            }
+            )
+        } catch (err) {
+            console.error(err);
         }
-
-        const data = await response.json();
-        console.log(data.categories);
-    } catch (err) {
-        console.error(err);
     }
 };
-btn.addEventListener("click", handleSearch)
+
+form.addEventListener("submit", handleSearch)
+
