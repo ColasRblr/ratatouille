b946d97c84549d02cb861c1c66c208b57001f3cf
Aude SEGUIN <seguin.aude08@gmail.com>
2023-09-30 14:09:52 +0200

    ajout affichage recette

--- detail.js
+++ detail.js
@@ -1,49 +1,49 @@
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
-let tabIngredient = []
+const id = params.get("id");
+let tabIngredient = [];
 
-console.log(id)
+console.log(id);
 
 const displayDetails = async (e) => {
-    try {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP! Statut: ${response.status}`);
-        }
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+    }
 
-        const data = await response.json();
-        for (const [key, value] of Object.entries(data.meals[0])) {
-            if (key.startsWith("strIngredient") && (value !== "")) {
-                tabIngredient[key] = value;
-            }
-        }
-        console.log(tabIngredient)
-        let detailsContainer = document.getElementById("details-container");
-        detailsContainer.innerHTML = `
+    const data = await response.json();
+    for (const [key, value] of Object.entries(data.meals[0])) {
+      if (key.startsWith("strIngredient") && value !== "" && value !== null) {
+        tabIngredient[key] = value;
+      }
+    }
+    let detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML = `
         <h1>${data.meals[0].strMeal}</h1>
-        <img src="${data.meals[0].strMealThumb}" />
+        <img id="img-details" src="${data.meals[0].strMealThumb}" />
         <div id="ingredients">Ingrédients :</div>
-        `
-        let ingredients = document.querySelector("#ingredients")
+        <div>Recette : <br> ${data.meals[0].strInstructions}</div>
 
-        for (var key in tabIngredient) {
-            var value = tabIngredient[key];
-            ingredients.innerHTML += `
+        `;
+    let ingredients = document.querySelector("#ingredients");
+
+    for (var key in tabIngredient) {
+      var value = tabIngredient[key];
+      ingredients.innerHTML += `
             <div>
             ${value}
             </div>
-            `
-        }
-        console.log(data.meals)
-
-    } catch (err) {
-        console.error(err);
+            `;
     }
-}
+    console.log(data.meals);
+  } catch (err) {
+    console.error(err);
+  }
+};
 displayDetails();
